884db2f4257555cdc92400d178e2f7f86d0ca2f2
MatheusPetrachin <mrp.petrachin@gmail.com>
2023-12-23 23:34:03 -0300

    dev

--- IATec.Calendar.WebApi/src/app/DataService.ts
+++ IATec.Calendar.WebApi/src/app/DataService.ts
@@ -8,22 +8,24 @@ import { EventModel } from './models/eventmodel';
   providedIn: 'root'
 })
 export class DataService {
-
   constructor(private http: HttpClient,
     private configService: ApiConfigServiceService,) { }
 
-
-
   getListEventData(userId: string, date: Date): Observable<EventModel[]> {
-
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': localStorage.getItem('Authorization') ?? '',
       'userid': userId,
       'period': date.toUTCString()
     });
-
-
     return this.http.get<EventModel[]>(this.configService.apiUrl + "/Events", { headers });
+  }
+
+  SelectUsers() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('Authorization') ?? ''
+    });
+    return this.http.get<EventModel[]>(this.configService.apiUrl + "/User", { headers });
   }
 }
--- IATec.Calendar.WebApi/src/app/events/events.component.ts
+++ IATec.Calendar.WebApi/src/app/events/events.component.ts
@@ -3,15 +3,26 @@ import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../DataService';
 
+interface selectModel {
+  value: number;
+  viewValue: string;
+}
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
 export class EventsComponent {
-  toppings = new FormControl('');
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
+  constructor(private router: Router, private dataService: DataService) {
+    this.getHours();
+    this.getMinutes();
+    //pegar usaurios para popular select
+  }
+
+  toppings = new FormControl('');
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
   hours: selectModel[] = [];
   minutes: selectModel[] = [];
 
@@ -27,19 +38,7 @@ export class EventsComponent {
     }
   }
 
-  constructor(private router: Router, private dataService: DataService) {
-    this.getHours();
-    this.getMinutes();
-
-    //pegar usaurios para popular select
-  }
-
   redirect(page: string) {
     this.router.navigate(['/' + page]);
   }
-}
-
-interface selectModel {
-  value: number;
-  viewValue: string;
 }
