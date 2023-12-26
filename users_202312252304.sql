9e912d8f59a40300291e641e1fb43154756a7c67
MatheusPetrachin <mrp.petrachin@gmail.com>
2023-12-25 23:06:05 -0300

    add init users

---
+++ users_202312252304.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+    "identity".users (
+        "Id",
+        "Name",
+        "Email",
+        "Password"
+    )
+VALUES (
+        '1fe28208-5574-4369-8892-ae87ea795f00',
+        'Matheus',
+        'matheus@gmail.com',
+        '123456'
+    ), (
+        '796875af-cd3d-43d0-8c11-0b8a7eb58326',
+        'Sara',
+        'sara@gmail.com',
+        '123456'
+    ), (
+        'c51e5880-04dd-4a21-a48a-1aa683e5793f',
+        'Eliane',
+        'eliane@gmail.com',
+        '123456'
+    ), (
+        '4a011a18-2bfb-41f6-85b4-7f855dca5fd3',
+        'Juliana',
+        'juliana@gmail.com',
+        '123456'
+    ), (
+        '0bdcc5d2-c085-4624-8a87-73dcbf78946d',
+        'José',
+        'jose@gmail.com',
+        '123456'
+    ),
+;
