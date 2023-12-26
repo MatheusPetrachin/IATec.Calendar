b85fb78a1173e15459a8f23895d35990252bbe06
MatheusPetrachin <mrp.petrachin@gmail.com>
2023-12-26 09:28:30 -0300

    docekr file backend

--- IATec.Calendar.Service/Startup.cs
+++ IATec.Calendar.Service/Startup.cs
@@ -97,7 +97,7 @@ namespace IATec.Calendar
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Context dataContext)
         {
             if (env.IsDevelopment())
             {
@@ -116,6 +116,8 @@ namespace IATec.Calendar
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "IATEC Calendar");
             });
+
+            dataContext.Database.Migrate();
 
             app.UseHttpsRedirection();
 
